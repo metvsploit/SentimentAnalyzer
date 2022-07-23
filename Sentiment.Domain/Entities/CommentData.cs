92550cc2ce3d9c09e4a9cb0661a69e277b3f8a36
metvsploit <dkwathout@yandex.ru>
2022-07-23 22:33:16 +0500

    Добавлена сущность комментария

---
+++ Sentiment.Domain/Entities/CommentData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentiment.Domain.Entities
+{
+    public class CommentData
+    {
+        public string? Person { get; set; }
+        public string? Comment { get; set; }
+        public bool IsPositive { get; set; }
+        public string? PersonUrl { get; set; }
+        public string? Published { get; set ; }
+        public int LikeCount { get; set; }
+    }
+}
