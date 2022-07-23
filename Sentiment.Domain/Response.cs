45c54c46fbce7c4b7bd215ce9f093665abe0aa35
metvsploit <dkwathout@yandex.ru>
2022-07-23 22:34:45 +0500

    Внесены изменения в сущность Response

--- Sentiment.Domain/Response.cs
+++ Sentiment.Domain/Response.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Sentiment.Domain.Entities;
 
 namespace Sentiment.Domain
 {
     public class Response
     {
         public bool Success { get; set; }
-        public List<string>? Comments { get; set; }
+        public List<CommentData>? Comments { get; set; }
         public string? Message { get; set; }
     }
 }
