57df7af97be24755874c0f95d7a7717448ed1e13
metvsploit <dkwathout@yandex.ru>
2022-07-23 17:19:30 +0500

    Cоздание модели

---
+++ Sentiment.PreparingModel/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
+namespace Sentiment.PreparingModel
+{
+    internal class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "data.txt");
+
+        static void Main(string[] args)
+        {
+
+            MLContext mlContext = new MLContext();
+            TrainTestData splitDataView = LoadData(mlContext);
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            Evaluate(mlContext, model, splitDataView.TestSet);
+
+
+            Console.WriteLine();
+            Console.WriteLine("=============== End of process ===============");
+
+            var dataview = GetDataView(mlContext);
+
+            using (var stream = System.IO.File.Create("mymodel.zip"))
+                mlContext.Model.Save(model, dataview.Schema, stream);
+
+            ITransformer newModel;
+            MLContext context = new MLContext();
+            DataViewSchema modelSchema;
+
+            using (var stream = System.IO.File.OpenRead("mymodel.zip"))
+            {
+                newModel = context.Model.Load(stream, out modelSchema);
+            }
+        }
+
+        public static IDataView GetDataView(MLContext mlContext)
+        {
+            return mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+        }
+
+        public static TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;
+        }
+
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            return model;
+        }
+
+        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+    }
+}
