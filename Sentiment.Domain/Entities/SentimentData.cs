ac800cd0e74055a86bdc7ad94b9b16d97e7f8ce4
metvsploit <dkwathout@yandex.ru>
2022-07-23 17:11:48 +0500

    Добавлены сущности

---
+++ Sentiment.Domain/Entities/SentimentData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace Sentiment.Domain.Entities
+{
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string? SentimentText { get; set; }
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment { get; set; }
+    }
+}
---
+++ Sentiment.Domain/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentiment.Domain
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public List<string>? Comments { get; set; }
+        public string? Message { get; set; }
+    }
+}
